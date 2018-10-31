ee4f84113c8e72096cf71984ecac8df5cd00b448
Norman Buruchara <buruch1@stolaf.edu>
2018-10-31 14:10:17 -0500

    Code for Freshwind Villas Website
    First push

---
+++ home.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Freshwind Villas</title>
+	 <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet'>
+</head>
+<body>
+
+		  <!-- Navigation --> 
+<nav class="navbar navbar-default navbar-fixed-top wow flipInX" id="my-navbar" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="" class="navbar-brand navbar-left">Freshwind Villas</a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbar-collapse" >
+        <ul class="nav navbar-nav navbar-right"> 
+          <li><a href="#facilities">Facilities</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#map">Location</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+</nav>
+<!-- End Navigation --> 
+
+
+<!-- Header Display -->
+<div class="jumbotron wow fadeInLeft" data-wow-delay="1.0s"> 
+		<div class="jcontainer1 text-center wow fadeInLeft" data-wow-delay="1.8s">
+			<h1>Freshwind Villas</h1>
+			<p><span class="class wow fadeInRight" data-wow-delay="3s">Your home away from home</span></p>
+		</div>
+	</div>
+	<!-- End Header Display -->
+
+<!---- Facilities --->
+<div class="conFacility" id="facilities">
+  <div class="row">
+    <h2>Facilities</h2>
+    <div class="pic1 col-lg-4">
+      <h2> </h2>
+      <img src="Images/garden.JPG">
+      <h3>Accomodation</h3>
+       <p>At legere feugiat antiopam vix. Sit ad eripuit saperet invidunt, qui vocent fastidii dissentiet ei. Ut pro mentitum mandamus, ne vim duis aliquid. Pro affert mentitum perpetua ea. No eos clita verear repudiare, sed esse augue delicata an, cu illum saperet conceptam qui.</p>
+    </div>
+    <div class="pic2 col-lg-4">
+      <h2></h2>
+      <img src="Images/double.JPG">
+      <h3>Conferencing</h3>
+      <p>At legere feugiat antiopam vix. Sit ad eripuit saperet invidunt, qui vocent fastidii dissentiet ei. Ut pro mentitum mandamus, ne vim duis aliquid. Pro affert mentitum perpetua ea. No eos clita verear repudiare, sed esse augue delicata an, cu illum saperet conceptam qui.</p>
+    </div>
+    <div class="pic3 col-lg-4">
+      <h2> </h2>
+      <img src="Images/sitting.JPG">
+      <h3>Events</h3>
+       <p>At legere feugiat antiopam vix. Sit ad eripuit saperet invidunt, qui vocent fastidii dissentiet ei. Ut pro mentitum mandamus, ne vim duis aliquid. Pro affert mentitum perpetua ea. No eos clita verear repudiare, sed esse augue delicata an, cu illum saperet conceptam qui.</p>
+    </div>
+  </div>
+</div>
+<!---- End Facilities --->
+
+<!---- Gallery ----->
+<div class="conGallery" id="gallery">
+    <section>
+      <div class="page-header" id="gallery"> 
+        <h2>Gallery</h2> 
+      </div>
+      <div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#screenshot-carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="1"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="2"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="3"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="4"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="5"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="6"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="7"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="8"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="9"></li>
+          <li data-target="#screenshot-carousel" data-slide-to="10"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="Images/house-out-zoom.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Mother's laugh</h3>
+              <p>This is my mother laughing</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Images/house-out-zoom2.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Images/master.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Brother's laugh</h3>
+              <p>This is my brother laughing</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Images/master2.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+           <div class="item">
+            <img src="Images/dining.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Images/sitting.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Images/sit-dining.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Images/stair-view.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+            <div class="item">
+            <img src="Images/play-frame.JPG" alt="Text of the image">
+            <div class="carousel-caption">
+              <h3>Sister's laugh</h3>
+              <p>This is my sister laughing</p>
+            </div>
+          </div>
+        </div>
+        <a href="#screenshot-carousel" class="left carousel-control" data-slide="prev" >
+          <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a href="#screenshot-carousel" class="right carousel-control" data-slide="next" >
+          <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+      </div>
+    </section>  
+  </div>
+<!---- End Gallery ----->
+
+<!---- Map -----> 
+<div class="conMap" id="map">
+  <h2> Location </h2>
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-6">
+      <img src="Images/map.jpg">
+    </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+        <h4></h4>
+    </div>
+  </div>
+</div>
+<!---- End Map ----->
+
+<!--- Contact ----->
+
+<div class="conContact">
+  <h2>Contact</h2>
+  <main>
+    <form class="contact-form" action="contactform.php" method="post">
+      <input type="text" name="name" placeholder="Enter full name">
+      <br><br>
+      <input type="text" name="mail" placeholder="Enter your e-mail">
+      <br><br>
+      <input type="text" name="subject" placeholder="Subject of your message">
+      <br><br>
+      <textarea name="message" placeholder="Enter your booking inquiries here"></textarea>
+      <br>
+      <button type="submit" name="submit">Send</button>
+    </form>
+  </main>
+</div>
+<!--- End Contact ----->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
